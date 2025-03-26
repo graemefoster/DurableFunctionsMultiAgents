f51f00acc766b094d7cbb2d0e239f440fbc101bd
Graeme Foster <graemefoster@gmail.com>
2025-03-26 16:35:43 +0800

    Adding aspire

---
+++ MultiAgentHost/Program.cs
@@ -0,0 +1,13 @@
+using Projects;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var signalR = builder.AddProject<DurableAgentSignalR>("DurableAgentSignalR"); 
+
+var functions = builder.AddAzureFunctionsProject<DurableAgentFunctions>("DurableAgentFunctions")
+    .WaitFor(signalR);
+
+var client = builder.AddNpmApp("DurableAgentClient", "../storyteller.client", "dev")
+    .WaitFor(functions);
+
+builder.Build().Run();
