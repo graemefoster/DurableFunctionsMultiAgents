2a4872095398f2efe6031502f1e10f16d4c73da6
Graeme Foster <graemefoster@gmail.com>
2025-03-27 08:59:19 +0800

    Few more tweaks for aspire

--- DurableAgentFunctions/Program.cs
+++ DurableAgentFunctions/Program.cs
@@ -41,9 +41,4 @@ builder.Services.AddSingleton(chatClient);
 
 builder.ConfigureFunctionsWebApplication();
 
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
-
 builder.Build().Run();
